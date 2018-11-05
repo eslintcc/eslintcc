8d80614536ca0858fb9cf07e2e31fa3558733264
IgorNovozhilov <maildrakon@gmail.com>
2018-11-05 23:41:02 +0300

    Test Ranks

--- source/lib/rank.js
+++ source/lib/rank.js
@@ -1,35 +1,80 @@
 'use strict';
 
+const rankLabels = ['A', 'B', 'C', 'D', 'E', 'F'];
+
 
 class Ranks {
 
   get defaultRanks() {
-    // The maximum complexity value associated with the rank
+    // The maximum complexity value for rule, associated with the rank
     return {
-      A: 5,
-      B: 10,
-      C: 20,
-      D: 30,
-      E: 40,
-      F: Infinity
+      'complexity': {
+        A: 5,
+        B: 10,
+        C: 20,
+        D: 30,
+        E: 40,
+        F: Infinity
+      },
+      'max-depth': {
+        A: 2,
+        B: 3,
+        C: 4,
+        D: 5,
+        E: 6,
+        F: Infinity
+      },
+      'max-nested-callbacks': {
+        A: 3,
+        B: 5,
+        C: 10,
+        D: 20,
+        E: 30,
+        F: Infinity
+      },
+      'max-params': {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+        F: Infinity
+      }
     };
   }
 
-  constructor(customRanks = {}) {
+  constructor(customRulesRanks = {}) {
     const ranks = this.defaultRanks;
-    for (let rankName in customRanks) {
-      rankName = rankName.toUpperCase();
-      if (rankName in ranks) {
-        ranks[rankName] = Number(customRanks[rankName]);
+    for (const ruleID in customRulesRanks) {
+      if (ruleID in ranks) {
+        const customRanks = customRulesRanks[ruleID];
+        for (let rankName in customRanks) {
+          rankName = rankName.toUpperCase();
+          if (rankName in ranks) {
+            ranks[ruleID][rankName] = Number(customRanks[rankName]);
+          }
+        }
       }
     }
     this.ranks = ranks;
     Object.assign(this, ranks);
   }
 
-  getName(value) {
-    for (const name in this.ranks) {
-      if (value < this.ranks[name]) {
+  getValue(ruleID, value) {
+    const ranks = this.ranks[ruleID];
+    for (let i = 0; i < rankLabels.length; i++) {
+      const rankMaxValue = ranks[rankLabels[i]];
+      if (value <= rankMaxValue) {
+        const prevMaxValue = ranks[rankLabels[i - 1]] || 0;
+        return i + (value - prevMaxValue) / (rankMaxValue - prevMaxValue);
+      }
+    }
+  }
+
+  getLabel(ruleID, value) {
+    const ranks = this.ranks[ruleID];
+    for (const name in ranks) {
+      if (value < ranks[name]) {
         return name;
       }
     }
