bd50f05b6a3a3c1874cf869ada39a676cc00f892
ia.novozhilov <ia.novozhilov@tensor.ru>
2018-11-19 13:12:17 +0300

    Выровнял текст сообщений в рамках файла

--- source/lib/logging.js
+++ source/lib/logging.js
@@ -31,15 +31,24 @@ class ReportLogger {
   finishFile(fileReport) {
     if (this.options.format === 'text' && fileReport.messages.length > 0) {
       console.log(relative(this.options.cwd, fileReport.fileName));
-      for (let lineIndex = 0; lineIndex < fileReport.messages.length; lineIndex++) {
+      let padStart = 0;
+      let padEnd = 0;
+      for (const { loc: { start: { line, column } } } of fileReport.messages) {
+        if (line > padStart) padStart = line;
+        if (column > padEnd) padEnd = column;
+      }
+      padStart = String(padStart).length;
+      padEnd = String(padEnd).length;
+      for (const message of fileReport.messages) {
         const {
           maxLabel,
           loc: { start: { line, column } },
           namePath,
           maxRuleId,
           maxRuleValue
-        } = fileReport.messages[lineIndex];
-        let text = `  ${this.colors[maxLabel]} ${line}:${column} ${namePath}`;
+        } = message;
+        const locStart = `${String(line).padStart(padStart)}:${String(column).padEnd(padEnd)}`;
+        let text = `  ${this.colors[maxLabel]} ${locStart} ${namePath}`;
         if (this.options.showRules) {
           text += ` (${maxRuleId} = ${maxRuleValue})`;
         }
