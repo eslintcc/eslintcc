fed5e5d9ce14e005dc9a2675db40b4cc37fc987a
ia.novozhilov <ia.novozhilov@tensor.ru>
2019-02-27 19:05:56 +0300

    test: maxRank + greaterThan

---
+++ test/src/complexity__max_average_rank_3_5.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function MyFunc(a, b, c) {
+  console.log(a, b, c);
+}
+
+MyFunc();
+
+
+function MyFunc2(a) {
+  if (a) {
+    if (a) {
+      if (a) {
+        if (a) {
+          if (a) {
+            if (a) {
+              if (a) {
+                if (a) {
+                  if (a) {
+                    if (a) {
+                      if (a) {
+                        console.log(a);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+
+MyFunc2();
