866c3a32d41c8f9e5e429d9cc2c6279357cf6dfd
IgorNovozhilov <maildrakon@gmail.com>
2021-01-11 22:22:50 +0300

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+'use strict';
+
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 
 module.exports = [{
   input: 'node_modules/@nodutilus/test/test.js',
   output: { file: 'build/@nodutilus-test.js', format: 'commonjs' },
   plugins: [nodeResolve()]
-}]
+}];
